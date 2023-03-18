f0d8aecdbd5b1f9509b7334bc7c53faa36cee5d4
NDRAEY <pikachu_andrey@vk.com>
2023-03-18 02:40:35 -0500

    Add print_number

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,33 @@ mod log;
 
 use multiboot::MultibootHeader;
 
+fn print_number(num: isize) {
+	let mut buf: [u8; 11] = [0; 11];
+    let mut n = num;
+	let mut idx = 0;
+
+    if n < 0 {
+        log::log::debug_write_char(b'-');
+        n = -n;
+    }
+
+    while n >= 10 {
+    	buf[idx] = b'0' + (n % 10) as u8;
+    	idx += 1;
+		n /= 10;
+    }
+
+   	buf[idx] = b'0' + (n % 10) as u8;
+	idx += 1;
+
+    while idx > 0 {
+    	log::log::debug_write_char(buf[idx]);
+    	idx -= 1;
+    }
+    
+   	log::log::debug_write_char(buf[idx]);
+}
+
 #[no_mangle]
 pub extern "C" fn _start(multiboot2_stack: u32, multiboot_structure_addr: u32) -> ! {
     debug!("Hello world from Rust!");
@@ -23,6 +50,8 @@ pub extern "C" fn _start(multiboot2_stack: u32, multiboot_structure_addr: u32) -
 
     let mb: *mut MultibootHeader = multiboot_structure_addr as *mut MultibootHeader;
     let addr = unsafe { (*mb).framebuffer_addr };
+
+    print_number(-12345678);
 
     // let width = unsafe { (*mb).framebuffer_width } as usize;
     // let height = unsafe { (*mb).framebuffer_height } as usize;
@@ -44,7 +73,7 @@ pub extern "C" fn _start(multiboot2_stack: u32, multiboot_structure_addr: u32) -
 
 #[lang = "eh_personality"]
 #[no_mangle]
-extern "C" fn eh_personality() {}
+extern "C" fn __eh_personality() {}
 
 #[panic_handler]
 #[no_mangle]
